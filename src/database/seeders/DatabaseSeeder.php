82b23330b0bed54678398de5566ef91962405f44
udacchi <takashi.19740727@gmail.com>
2025-06-05 19:57:18 +0900

    ダミーデータ作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -11,8 +11,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(CategorySeeder::class);
+        $this->call([
+            CategorySeeder::class,
+            ContactSeeder::class,
+        ]);
     }
 }
